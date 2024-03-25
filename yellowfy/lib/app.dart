6539a46c9b21de72d0e14d4f5ed3bb70c9f8d3ea
Afonso Rodrigues <afonsocr@ua.pt>
2024-03-25 12:50:22 +0000

    Main Page changes

--- yellowfy/lib/app.dart
+++ yellowfy/lib/app.dart
@@ -63,75 +63,55 @@ class MainPage extends StatelessWidget {
             ),
           ],
         ),
-        bottom: PreferredSize(
-          preferredSize: Size.fromHeight(1.0),
-          child: Divider(color: Colors.black.withOpacity(1)),
-        ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 50),
-        child: Container(
-          decoration: BoxDecoration(
-            border: Border.all(color: Colors.black),
-            borderRadius: BorderRadius.circular(10),
+      body: GoogleMap(
+        initialCameraPosition:
+          CameraPosition(target: LatLng(40.6011, -8.67039), zoom: 11.05),
+        zoomControlsEnabled: true,
+        myLocationButtonEnabled: false,
+        myLocationEnabled: true,
+        markers: {
+          Marker(
+            markerId: MarkerId("1"),
+            position: LatLng(40.6332, -8.6595),
+            icon: BitmapDescriptor.defaultMarkerWithHue(
+              BitmapDescriptor.hueAzure),
+            infoWindow: InfoWindow(
+              title: "DETI",
+                snippet: "Central da depressão",
+            ),
           ),
-          child: GoogleMap(
-            initialCameraPosition:
-                CameraPosition(target: LatLng(40.6011, -8.67039), zoom: 11.05),
-            zoomControlsEnabled: false,
-            myLocationButtonEnabled: false,
-            myLocationEnabled: true,
-            compassEnabled: false,
-            mapToolbarEnabled: false,
-            zoomGesturesEnabled: false,
-            rotateGesturesEnabled: false,
-            scrollGesturesEnabled: false,
-            tiltGesturesEnabled: false,
-            markers: {
-              Marker(
-                markerId: MarkerId("1"),
-                position: LatLng(40.6332, -8.6595),
-                icon: BitmapDescriptor.defaultMarkerWithHue(
-                    BitmapDescriptor.hueAzure),
-                infoWindow: InfoWindow(
-                  title: "DETI",
-                  snippet: "Central da depressão",
-                ),
-              ),
-              Marker(
-                markerId: MarkerId("2"),
-                position: LatLng(40.6364, -8.6531),
-                icon: BitmapDescriptor.defaultMarkerWithHue(
-                    BitmapDescriptor.hueAzure),
-                infoWindow: InfoWindow(
-                  title: "Parque da Macaca",
-                  snippet: "Central do Assalto",
-                ),
-              ),
-              // make another marker for praça do peixe in aveiro
-              Marker(
-                markerId: MarkerId("3"),
-                position: LatLng(40.6441, -8.6564),
-                icon: BitmapDescriptor.defaultMarkerWithHue(
-                    BitmapDescriptor.hueAzure),
-                infoWindow: InfoWindow(
-                  title: "Ponte do laço",
-                  snippet: "Che grande beijo",
-                ),
-              ),
-              Marker(
-                markerId: MarkerId("3"),
-                position: LatLng(40.6341, -8.6600),
-                icon: BitmapDescriptor.defaultMarkerWithHue(
-                    BitmapDescriptor.hueAzure),
-                infoWindow: InfoWindow(
-                  title: "IT",
-                  snippet: "Money Grabers",
-                ),
-              ),
-            },
+          Marker(
+            markerId: MarkerId("2"),
+            position: LatLng(40.6364, -8.6531),
+            icon: BitmapDescriptor.defaultMarkerWithHue(
+              BitmapDescriptor.hueAzure),
+            infoWindow: InfoWindow(
+              title: "Parque da Macaca",
+              snippet: "Central do Assalto",
+            ),
           ),
-        ),
+          Marker(
+            markerId: MarkerId("3"),
+            position: LatLng(40.6441, -8.6564),
+            icon: BitmapDescriptor.defaultMarkerWithHue(
+                BitmapDescriptor.hueAzure),
+            infoWindow: InfoWindow(
+              title: "Ponte do laço",
+              snippet: "Che grande beijo",
+            ),
+          ),
+          Marker(
+            markerId: MarkerId("3"),
+            position: LatLng(40.6341, -8.6600),
+            icon: BitmapDescriptor.defaultMarkerWithHue(
+              BitmapDescriptor.hueAzure),
+            infoWindow: InfoWindow(
+              title: "IT",
+              snippet: "Money Grabers",
+            ),
+          ),
+        },
       ),
     );
   }
