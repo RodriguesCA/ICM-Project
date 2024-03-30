2a07be5475ee4513eb73d1f0e20ad3a68b21ae8a
Gmclaro <gui@ua.pt>
2024-03-30 13:33:27 +0000

    cards done , qr code scanner to do

--- yellowfy/lib/app.dart
+++ yellowfy/lib/app.dart
@@ -3,9 +3,48 @@ import 'package:provider/provider.dart';
 import "package:google_maps_flutter/google_maps_flutter.dart";
 import "package:yellowfy/welcome.dart";
 import "package:yellowfy/shop.dart";
+import 'package:qr_flutter/qr_flutter.dart';
 
 void main() {
   runApp(const MyApp());
+}
+
+void _showMarkerPopup(BuildContext context, String title, String description) {
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text(title),
+        content: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(description),
+            SizedBox(
+                height: 10), // Optional spacing between description and button
+            ElevatedButton(
+              onPressed: () {
+                // Navigate to QR code scanner page
+                // Navigator.push(
+                //context,
+                // MaterialPageRoute(builder: (context) => QRScannerPage()),
+                // );
+              },
+              child: Text('Scan QR Code'),
+            ),
+          ],
+        ),
+        actions: <Widget>[
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: Text('Close'),
+          ),
+        ],
+      );
+    },
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -40,53 +79,53 @@ class MyAppState extends ChangeNotifier {
 // ignore: must_be_immutable
 class MainPage extends StatelessWidget {
   List<LatLng> polygonPointsIT = [
-    LatLng(40.63787867676456, -8.65856742392844),  // top left corner
-    LatLng(40.63361546575474, -8.661243742835701),  // bottom left corner
-    LatLng(40.62004155057745, -8.658948487500192),  // bottom right corner
-    LatLng(40.62452855358944, -8.64976460361052),   // top right corner
+    LatLng(40.63787867676456, -8.65856742392844), // top left corner
+    LatLng(40.63361546575474, -8.661243742835701), // bottom left corner
+    LatLng(40.62004155057745, -8.658948487500192), // bottom right corner
+    LatLng(40.62452855358944, -8.64976460361052), // top right corner
   ];
   List<LatLng> polygonPointsParqueMacaca = [
-    LatLng(40.63272744890467, -8.64855518938427),  // top right corner
-    LatLng(40.63338985277366, -8.648786538128617),  // extra
-    LatLng(40.6399927705059, -8.655116460602876),  // top left corner
-    LatLng(40.63787867676456, -8.65856742392844),  // bottom left corner
-    LatLng(40.63056521992064, -8.654170248456797),  // bottom right corner
+    LatLng(40.63272744890467, -8.64855518938427), // top right corner
+    LatLng(40.63338985277366, -8.648786538128617), // extra
+    LatLng(40.6399927705059, -8.655116460602876), // top left corner
+    LatLng(40.63787867676456, -8.65856742392844), // bottom left corner
+    LatLng(40.63056521992064, -8.654170248456797), // bottom right corner
   ];
   List<LatLng> polygonPointsSalinas = [
-    LatLng(40.64584894137766, -8.662885293949005),  // top left corner
-    LatLng(40.6366802512532, -8.686951856569015),   // extra
-    LatLng(40.63755790248674, -8.687716520992657),  // extra
-    LatLng(40.63754339594855, -8.6886914681328),    // extra
+    LatLng(40.64584894137766, -8.662885293949005), // top left corner
+    LatLng(40.6366802512532, -8.686951856569015), // extra
+    LatLng(40.63755790248674, -8.687716520992657), // extra
+    LatLng(40.63754339594855, -8.6886914681328), // extra
     LatLng(40.626249207879724, -8.682347226061006), // bottom right corner
-    LatLng(40.63787867676456, -8.65856742392844),   // top right corner
+    LatLng(40.63787867676456, -8.65856742392844), // top right corner
     LatLng(40.639593752531106, -8.655944809760609), // extra
   ];
   List<LatLng> polygonPointsBarra = [
-    LatLng(40.63754339594855, -8.6886914681328),    // top right corner
+    LatLng(40.63754339594855, -8.6886914681328), // top right corner
     LatLng(40.630624731442886, -8.750970539177398), // bottom right corner
-    LatLng(40.640469790728964, -8.75011962660272),  // extra
-    LatLng(40.64205154345762, -8.758143275839053),  // extra
+    LatLng(40.640469790728964, -8.75011962660272), // extra
+    LatLng(40.64205154345762, -8.758143275839053), // extra
     LatLng(40.642417082386906, -8.757913786302257), // extra
-    LatLng(40.64075503936687, -8.750588131372883),  // extra
-    LatLng(40.64413984641427, -8.749853123659687),  // bottom left corner
-    LatLng(40.65825001627285, -8.719445773361878),  // extra
-    LatLng(40.65896005972578, -8.709161235108828),  // extra
-    LatLng(40.65476769594774, -8.702753020122302),  // extra
+    LatLng(40.64075503936687, -8.750588131372883), // extra
+    LatLng(40.64413984641427, -8.749853123659687), // bottom left corner
+    LatLng(40.65825001627285, -8.719445773361878), // extra
+    LatLng(40.65896005972578, -8.709161235108828), // extra
+    LatLng(40.65476769594774, -8.702753020122302), // extra
   ];
   List<LatLng> polygonPointsCostaNova = [
-    LatLng(40.63754339594855, -8.6886914681328),    // top left corner
+    LatLng(40.63754339594855, -8.6886914681328), // top left corner
     LatLng(40.630624731442886, -8.750970539177398), // bottom left corner
-    LatLng(40.608877485635745, -8.75648653881182),  // bottom right corner
-    LatLng(40.6005038853122, -8.690439825852636),   // top right corner
+    LatLng(40.608877485635745, -8.75648653881182), // bottom right corner
+    LatLng(40.6005038853122, -8.690439825852636), // top right corner
     LatLng(40.626249207879724, -8.682347226061006), // extra
   ];
   List<LatLng> polygonPointsPassadicos = [
     LatLng(40.626249207879724, -8.682347226061006), // bottom right corner
-    LatLng(40.6005038853122, -8.690439825852636),   // extra
+    LatLng(40.6005038853122, -8.690439825852636), // extra
     LatLng(40.615252910083306, -8.677101677162222), // bottom left corner
-    LatLng(40.62004155057745, -8.658948487500192),  // top left corner
-    LatLng(40.63361546575474, -8.661243742835701),  // extra
-    LatLng(40.63787867676456, -8.65856742392844),  // top right corner
+    LatLng(40.62004155057745, -8.658948487500192), // top left corner
+    LatLng(40.63361546575474, -8.661243742835701), // extra
+    LatLng(40.63787867676456, -8.65856742392844), // top right corner
   ];
   // AFONSO ADICIONA AQUI OS OUTROS ARRAYS QUE FALTAM
 
@@ -131,6 +170,10 @@ class MainPage extends StatelessWidget {
             infoWindow: InfoWindow(
               title: "Universidade de Aveiro",
             ),
+            onTap: () {
+              _showMarkerPopup(context, "Universidade de Aveiro",
+                  "Description of the University");
+            },
           ),
           Marker(
             markerId: MarkerId("2"),
@@ -207,7 +250,7 @@ class MainPage extends StatelessWidget {
           // Adicionar Fábrica, Praça, Estação (?)
         },
         polygons: {
-           Polygon(
+          Polygon(
             polygonId: PolygonId("1"),
             points: polygonPointsIT,
             fillColor: Colors.grey,
